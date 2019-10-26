69c4e3ab5929e1817d17a498e0a1c42f113b4610
JosephLindzius <joseph.lindzius@gmail.com>
2019-10-26 12:13:39 +0200

    navbar

--- public/assets/scripts/scriptDashboard.js
+++ public/assets/scripts/scriptDashboard.js
@@ -1,10 +1,10 @@
-photoArray = ["./public/assets/img/0.svg", "./public/assets/img/1.svg", "./public/assets/img/2.svg"];
+photoArray = ["./assets/img/0.svg", "./assets/img/1.svg", "./assets/img/2.svg"];
 let i = 0;
 setInterval(function() {
     console.log("soloHouse");
-    const img = document.getElementById('soloHouse');
+    let img = document.getElementById('soloHouse');
     img.src = photoArray[i];
-    console.log("img.src");
+    console.log(photoArray[i]);
     i++;
     if (i > 2) {
          i = 0;
