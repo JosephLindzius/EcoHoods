e089a44fece7bf681d573ac09ecced5afc31f828
JosephLindzius <joseph.lindzius@gmail.com>
2019-10-26 13:55:44 +0200

    merging css

--- public/assets/scripts/scriptDashboard.js
+++ public/assets/scripts/scriptDashboard.js
@@ -1,10 +1,8 @@
 photoArray = ["./assets/img/0.svg", "./assets/img/1.svg", "./assets/img/2.svg"];
 let i = 0;
 setInterval(function () {
-    console.log("smartHouse");
     const img = document.getElementById('smartHouse');
     img.src = photoArray[i];
-    console.log("img.src");
     i++;
     if (i > 2) {
         i = 0;
